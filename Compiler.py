401dffc3435b07a853ee063aca5d3adf88cb20ad
Itsfizziks <55935361+itsfizziks@users.noreply.github.com>
2023-03-03 23:53:44 -0500

    Add files via upload
    This version introduces (untested) the username finder feature. Provide the last name of the desired user in the prompt and the program will do the rest! - has yet to be tested on real terrain...

--- Compiler.py
+++ Compiler.py
@@ -5,12 +5,12 @@ from tkinter import simpledialog;
 
 root = tk.Tk()
 root.title("Active Directory - QOL dlc")
-root.geometry("700x350")
-tk.Label(root,text="Active directory fancy search engine").pack()
+root.geometry("350x350")
+tk.Label(root,text="Active directory fancy search engine").pack(side=tk.TOP)
 
 input = tk.Entry()
 input.insert(0,"B or K number")
-input.pack()
+input.pack() 
 
 
 
@@ -36,13 +36,33 @@ def passwrd_exp():
     newWindow.geometry("500x200")
     tk.Label(newWindow,text =rez).pack()
 
-def update():
+def get_Username():
+    def run_userfinder():
+        ps_command = 'Get-ADUser -Filter Surname -eq "{}"| Select-Object -Property SamAccountName'.format(input.get())
+        ps_output = subprocess.check_output(["powershell.exe", "-Command", ps_command])
+        rez = (ps_output.decode("utf-8"))
+        tk.Label(newWindow,text=rez).pack()
+        
     newWindow = tk.Toplevel()
-    newWindow.title("update")
+    newWindow.title("Whats the username?")
     newWindow.geometry("500x200")
-    tk.Label(newWindow,text =input.get()).pack()
+    tk.Label(newWindow,text ="Username Finder (only from last name - BETA)").pack()
+    input = tk.Entry(newWindow)
+    input.insert(0,"enter user's last name")
+    input.pack()
+    button_run = tk.Button(newWindow,text="run",command=run_userfinder,fg="green")
+    button_run.pack()
+   
+    
 
+#def update():
+    #newWindow = tk.Toplevel()
+    #newWindow.title("update")
+    #newWindow.geometry("500x200")
+    #tk.Label(newWindow,text =input.get()).pack()
 
+def exit():
+    root.quit()
 
 # Buttons
 button_Group = tk.Button(root, text="Group search", command=group_of)
@@ -51,8 +71,14 @@ button_Group.pack()
 button_Passwrd = tk.Button(root, text="Password check-up", command=passwrd_exp)
 button_Passwrd.pack()
 
-button_Update = tk.Button(root,text="check", command=update)
-button_Update.pack()
+button_Userfinder = tk.Button(root,text="Username finder - BETA", command=get_Username)
+button_Userfinder.pack()
+
+#button_Update = tk.Button(root,text="", command=update)
+#button_Update.pack()
+
+button_Exit = tk.Button(root,text="Exit",command=exit,fg="red")
+button_Exit.pack(side=tk.BOTTOM)
 
 # Start the event loop
 root.mainloop()
